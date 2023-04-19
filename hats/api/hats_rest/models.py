e96a8c08c9227f76b79bed898f39b31dc05b3b1a
Michael Gianoulakis <mgiano02@gmail.com>
2023-04-19 15:15:17 -0700

    Added hats models and made migrations

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class LocationVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    section_number = models.PositiveSmallIntegerField()
+    shelf_number = models.PositiveSmallIntegerField()
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=100)
+    style_name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    picture_url = models.URLField(max_length=200)
+
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="location",
+        on_delete=models.CASCADE
+    )
