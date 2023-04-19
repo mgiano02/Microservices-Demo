201de9f6a90d5840b593e83eac02fb477d73fc6a
Sina Klughardt <sina.klughardt@gmail.com>
2023-04-19 15:15:22 -0700

    Wrote the models for Shoe and BinVO

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class BinVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    bin_number = models.PositiveSmallIntegerField()
+    bin_size = models.PositiveSmallIntegerField()
+
+class Shoe(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    model_name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    picture = models.URLField()
+    bin = models.ForeignKey(
+        BinVO,
+        related_name="shoe",
+        on_delete=models.CASCADE
+    )
